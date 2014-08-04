d34d4b5720bb7930ba71a237154d9b7b58d141d8
Nick Overdijk <nick@astrant.net>
2014-08-04 14:28:26 +0200

    Now accepts first commandline arg as rom, checks for boot_rom.bin existance

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 #include "gameboy.h"
 #include <iostream>
+#include <boost/filesystem.hpp>
 
-int main( int argc, char* args[] )
+int main( int argc, char* argv[] )
 {
-    gameboy_t gb(true, "01-special.gb");
-//    gameboy_t gb(false, "06-ld r,r.gb");
+    if(argc < 2){
+        std::cerr << "Usage: " << argv[0] << " <rom>" << std::endl;
+        return -1;
+    }
 
-//    gameboy_t gb(true, "cpu_instrs.gb");
+    bool const emulate_boot_rom = boost::filesystem::exists("boot_rom.bin");
+    gameboy_t gb(emulate_boot_rom, argv[1]);
+    gb.run();
 
-//	gameboy_t gb(true, "tetris.gb");
-//	gameboy_t gb(true, "pokemon_blue.gb");
-
-//	gameboy_t gb(true, "hello.gb");
-
-//    gameboy_t gb(false, "test.gb");
-	gb.run();
-
-	return 0;
+    return 0;
 }
